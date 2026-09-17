38a57b9a7f8c0853b1ac3ba39802d4a58253c858
Prashant Rao <prashant.rao-ext+hpeprod@hpe.com>
2026-09-17 21:29:38 +0530

    update the server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { buildDashboardSummary, normalizeRequirementInput } = require('./src/admi
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3200;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
